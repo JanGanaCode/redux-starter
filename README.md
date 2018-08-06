252c17e208e5038ee53b43ba5e65061b53816ed2
Jan Gana <jan.gana@hiring-hub.com>
2018-08-06 03:09:48 +0100

    Finished Project

--- README.md
+++ README.md
@@ -10,16 +10,11 @@ There are two methods for getting started with this repo.
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
+> git clone https://github.com/JanGanaCode/redux-starter.git
+> cd redux-starter
 > npm install
 > npm start
 ```
-
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
-> npm install
-> npm start
-```
+* sign up for YouTube API at Google Developers 
+* generate your own API_KEY
+* use it at: /src/index.js @ const API_KEY = '<your YouTube API key>'
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 // import React
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+const API_KEY = '';
 
 // create a new component which should produce some HTML
-const App = () => {
-  return <div>Hi</div>;
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    }
+
+    YTSearch({key: API_KEY, term: 'blink182'}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+    });
+  };
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          videos={this.state.videos}
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo })}
+        />
+      </div>
+    );
+  }
 }
 
 // take this component's generated HTML and put it on the page (in the DOM)
